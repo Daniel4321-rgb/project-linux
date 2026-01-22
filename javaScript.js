5806802962a9fb8ad63792170fc99fc67592be55
Daniel4321-rgb <dani2009g@icloud.com>
2026-01-22 14:06:52 +0200

    added java script file and made a button that chenges text

---
+++ javaScript.js
@@ -0,0 +1,11 @@
+let p = document.querySelector(".newText")
+
+function newButton()
+{
+    p.style.backgroundColor = "yellow";
+    p.style.borderStyle = "solid";
+    p.style.borderWidth  = "medium" ;
+    p.style.borderColor = "black";
+    p.style.borderRadius = "15px";
+    p.style.color = "blue";
+}
